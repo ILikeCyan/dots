63130ac6d9bbc038b7a373919c3876d87aa339dd
ILikeCyan <jordan.2121@live.com.au>
2024-10-08 22:41:25 +1100

    big update
    nvim

--- .config/nvim/lua/user/settings.lua
+++
@@ -1,9 +0,0 @@
--- Please check `lua/core/settings.lua` to view the full list of configurable settings
-local settings = {}
-
--- Examples
-settings["use_ssh"] = false
-
-settings["colorscheme"] = "catppuccin"
-
-return settings
