8a1755fff400295018da50b1c577f5e05707b33b
ILikeCyan <jordan.2121@live.com.au>
2024-11-23 07:38:24 +1100

    Update build.sh
    This is hrd on phone

--- build.sh
+++ build.sh
@@ -19,4 +19,14 @@ if [ $ans=1 ]
     cp -r $HOME/.config/hypr $HOME/.config/hypr.bak
     echo "Copying dots"
     cp -r .config/hypr $HOME/.config/hypr
+elif [ $ans=2 ]
+  then
+    echo "Backing up kittyand lf" ,
+    mkdir $HOME/.config/lf.bak
+    mkdir $HOME/.config/kitty.bak
+    cp -r $HOME/.config/lf $HOME/.config/lf.bak
+    cp -r $HOME/.config/kitty $HOME/.config/kitty.bak
+    echo "Copying dots"
+    cp -r .config/lf $HOME/.config/lf
+    cp -r .config/lf $HOME/.config/kitty
   fi
